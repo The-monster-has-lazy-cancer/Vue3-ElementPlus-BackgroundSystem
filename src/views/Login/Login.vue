64b49c4ac4b0503df067a9e582f73a0badbf02e0
le <tjlhs@outlook.com>
2023-05-02 00:19:43 +0800

    Login页面设计大体样式

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -2,70 +2,84 @@
 	<div class="LoginContainer">
 		<div class="Login">
 			<div class="L-left-Logo">
-				<img src="../../../public/static/img/System-Logo.png" alt="">
+				<img src="../../../public/static/img/System-Logo.png" alt="我是个logo">
 			</div>
 			<div class="L-right-Form">
-				
+				<div class="LoginTitle">
+					妖怪系统登录
+				</div>
+				<div class="Logincontent">
+					<el-form ref="form" :model="LoginForm" label-width="auto" :label-position="labelPosition"
+						:size="size">
+						<el-form-item>
+							<el-input v-model="LoginForm.Name" placeholder="请输入账户名">
+								<template #prepend>
+									<el-button :icon="User" />
+								</template>
+							</el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-input type="password" v-model="LoginForm.PassWord" placeholder="请输入密码">
+								<template #prepend>
+									<el-button :icon="Hide" />
+								</template>
+							</el-input>
+						</el-form-item>
+					</el-form>
+				</div>
 			</div>
 		</div>
 		<div class="footer">
-			&copy;{{Copyright}}
+			&copy;&nbsp;{{Copyright}}&nbsp;&nbsp;{{utils.Year}}
+
+
+			<div v-show="false" style="position: fixed;bottom: 0;right: 0;">
+				<el-radio-group v-model="size" label="size control">
+					<el-radio-button label="large">large</el-radio-button>
+					<el-radio-button label="default">default</el-radio-button>
+					<el-radio-button label="small">small</el-radio-button>
+				</el-radio-group>
+				<el-radio-group v-model="labelPosition" label="position control">
+					<el-radio-button label="left">Left</el-radio-button>
+					<el-radio-button label="right">Right</el-radio-button>
+					<el-radio-button label="top">Top</el-radio-button>
+				</el-radio-group>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup>
-	import {ref,reactive,onMounted} from 'vue'
+	import {
+		ref,
+		reactive,
+		onMounted
+	} from 'vue'
 	import utils from '../../utils/index.js'
-	// var date = new Date()
-	// var year = date.getFullYear()
+	import {
+		User,
+		Hide
+	} from '@element-plus/icons-vue'
 	const Copyright = ref('')
-	onMounted(()=>{
-		Copyright.value =utils.Year+'妖怪得懒癌'
+	onMounted(() => {
+		Copyright.value = '妖怪得懒癌管理系统'
 	})
+
+
+	// 提交框输入大小
+	const size = ref('large')
+	const labelPosition = ref('right')
+
+	const LoginForm = reactive({
+		Name: '',
+		PassWord: '',
+	})
+
+	function onSubmit() {
+		console.log('submit!')
+	}
 </script>
 
-<style scoped="scoped">
-	.LoginContainer{
-		width: 100vw;
-		height: 100vh;
-		position: relative;
-		background-color: #000000;
-	}
-	.footer{
-		color: #fff;
-		width: 100%;
-		text-align: center;
-		position: fixed;
-		bottom: 0;
-		
-	}
-	.LoginContainer .Login{
-		display:flex;
-		width: 35rem;
-		height: 25rem;
-		position: absolute;
-		left: 50%;
-		bottom:50%;
-		transform:translate(-50%,50%);
-		background-color: #fff;
-		border-radius: 10px;
-		overflow: hidden;
-	}
-	.LoginContainer .L-left-Logo{
-		flex: 4;
-		height: 100%;
-	}
-	.LoginContainer .L-left-Logo img{
-		width: 12.5rem;
-		height: 12.5rem;
-		border-radius: 50%;
-		border: 1px solid #f2f2f2;
-		box-shadow: #f2f2f2 2px 2px 6px,#fff 2px 2px -6px;
-	}
-	.LoginContainer .L-right-Form{
-		flex: 6;
-		height: 100%;
-		background-color: antiquewhite;
-		}
+<style>
+	@import url('../../styles/Login/Login.css');
 </style>
