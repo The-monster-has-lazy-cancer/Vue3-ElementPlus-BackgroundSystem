5a618bd4cc4216ac71a2f54590042b9bf442f233
le <tjlhs@outlook.com>
2023-05-04 00:02:41 +0800

    主页大概样子

---
+++ src/views/Index/Index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="common-layout">
+		<el-container>
+			<el-aside width="200px">Aside</el-aside>
+			<el-container>
+				<el-header>Header</el-header>
+				<el-main>Main</el-main>
+				<el-footer>Footer</el-footer>
+			</el-container>
+		</el-container>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style scoped>
+	* {
+		padding: 0;
+		margin: 0;
+	}
+
+	.common-layout,
+	.el-container {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
+
