c0bc46253ce9622ac28b55c5e2dafff523f9c577
妖怪得懒癌 <88729742+The-monster-has-lazy-cancer@users.noreply.github.com>
2023-05-10 00:45:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 //mode代表运行环境
 //loadEnv在congfig中加载环境变量
+//测试拉取
 export default({mode})=>{
 	const env = loadEnv(mode,process.cwd())
 	return defineConfig({
